8b764bc4ceda83fb479a59ee845fe5e7aabef6a8
Shivam Kumar Verma <101925998+shivamverma-9984@users.noreply.github.com>
2023-09-04 19:59:03 +0530

    Time: 416 ms (79.43%), Space: 0B (100.00%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,7 @@
+select *from Cinema where id%2!=0 and  description!='boring' order by rating desc;
+
+
+
+# SELECT * FROM Cinema WHERE id % 2 != 0 AND 
+# description != "boring" 
+# ORDER BY rating DESC ;
