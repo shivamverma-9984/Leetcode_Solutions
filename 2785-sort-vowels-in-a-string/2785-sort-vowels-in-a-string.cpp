e9c0975b5d0dfbe9947930aedbc39e938e7afbfd
Shivam Kumar Verma <101925998+shivamverma-9984@users.noreply.github.com>
2023-11-13 18:06:11 +0530

    Time: 39 ms (69.26%), Space: 12.1 MB (85.13%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    
+    bool isvowel(char ch)
+    {
+        ch=tolower(ch);
+        return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u');
+    }
     string sortVowels(string s) {
-        string t="",vow="";
-        int n=s.size(),i=0;
-        for(char x:s)
+        string temp;
+        for(auto ch:s)
         {
-            if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
-                vow+=x;
+            if(isvowel(ch))
+            {
+                temp+=ch;
+            }
         }
-        sort(vow.begin(),vow.end());
-        for(char x:s)
-        {
-            if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
-                t+=vow[i++];
-            else
-                t+=x;
+        sort(begin(temp),end(temp));
+        int j=0;
+        for(int i=0;i<s.size();i++){
+            if(isvowel(s[i]))
+            {
+                s[i]=temp[j];
+                j++;
+            }
         }
-        return t;
+        return s;
     }
 };
