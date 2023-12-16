9c2c6969f9a0829cdc291f5de9b70b651326bd94
Shivam Kumar Verma <101925998+shivamverma-9984@users.noreply.github.com>
2023-12-16 13:26:15 +0530

    Time: 73 ms (31.97%), Space: 45.1 MB (26.81%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    let res=[]
+    for(let i=0;i<words.length;i++)
+        {
+            if(words[i].includes(x)){
+                res.push(i);
+            }
+        }
+    return res;
+};
